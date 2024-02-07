f3869bc5aa3bec1514fcc0c4043ce8e07762804f
Alan <alancurtis5000+accounts@gmail.com>
2024-02-06 21:51:49 -0700

    added storage s3

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -16,5 +16,11 @@ export type AmplifyDependentResourcesAttributes = {
       "UserPoolId": "string",
       "UserPoolName": "string"
     }
+  },
+  "storage": {
+    "app02Friendly": {
+      "BucketName": "string",
+      "Region": "string"
+    }
   }
 }
